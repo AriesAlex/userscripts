d3869396eee678f3eaf3fa6a03780d23ad032560
ArieX <39626061+AriesAlex@users.noreply.github.com>
2025-09-08 18:12:45 +0300

    Update dtf-anti-adblock-banner.user.js

--- dtf-anti-adblock-banner.user.js
+++ dtf-anti-adblock-banner.user.js
@@ -2,7 +2,7 @@
 // @name         DTF Anti-AdBlock Banner
 // @namespace    https://github.com/AriesAlex/userscripts
 // @version      1.0.0
-// @description  Автоматически закрывает баннер 'Вы используете блокировщик рекламы' на DTF
+// @description  Автоматически закрывает баннер 'Вы используете блокировщик рекламы' на DTF. Надежная версия.
 // @author       AriesAlex
 // @match        *://*.dtf.ru/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=dtf.ru
@@ -16,31 +16,36 @@
     'use strict'
 
     const targetText = 'Вы используете блокировщик рекламы'
+    let pollingInterval = null
 
     const findAndCloseModal = () => {
-        const h1Elements = document.querySelectorAll('h1.blocker__title')
+        const h1 = document.querySelector('.modal-overlay:not([style*="display: none"]) h1.blocker__title')
 
-        for (const h1 of h1Elements) {
-            if (h1.textContent.includes(targetText)) {
-                const modalWindow = h1.closest('.modal-window')
-                if (!modalWindow) continue
+        if (h1 && h1.textContent.includes(targetText)) {
+            const modalWindow = h1.closest('.modal-window')
+            if (!modalWindow) return
 
-                const buttonToClick = modalWindow.querySelector('button.modal-window__back') || modalWindow.querySelector('button.modal-window__close')
+            const buttonToClick = modalWindow.querySelector('button.modal-window__back') || modalWindow.querySelector('button.modal-window__close')
 
-                if (buttonToClick) {
-                    buttonToClick.click()
-                    return
+            if (buttonToClick) {
+                buttonToClick.click()
+                if (pollingInterval) {
+                    clearInterval(pollingInterval)
+                    pollingInterval = null
                 }
+                return true
             }
         }
+        return false
     }
 
     const observer = new MutationObserver(findAndCloseModal)
-
     observer.observe(document.body, {
         childList: true,
         subtree: true
     })
+
+    pollingInterval = setInterval(findAndCloseModal, 500)
 
     findAndCloseModal()
 })()

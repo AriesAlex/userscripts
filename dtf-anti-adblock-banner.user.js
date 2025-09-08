e24b11676ba018076edad3c7b18299ef789a5951
ArieX <39626061+AriesAlex@users.noreply.github.com>
2025-09-08 18:03:59 +0300

    Create dtf-anti-adblock-banner.user.js

---
+++ dtf-anti-adblock-banner.user.js
@@ -0,0 +1,43 @@
+// ==UserScript==
+// @name         DTF Anti-AdBlock Banner
+// @namespace    https://github.com/AriesAlex/userscripts
+// @version      1.0.0
+// @description  Автоматически закрывает баннер 'Вы используете блокировщик рекламы' на DTF
+// @author       AriesAlex
+// @match        *://*.dtf.ru/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=dtf.ru
+// @grant        none
+// @run-at       document-start
+// @updateURL    https://raw.githubusercontent.com/AriesAlex/userscripts/main/dtf-anti-adblock-banner.user.js
+// @downloadURL  https://raw.githubusercontent.com/AriesAlex/userscripts/main/dtf-anti-adblock-banner.user.js
+// ==/UserScript==
+
+(() => {
+    'use strict'
+
+    const targetText = 'Вы используете блокировщик рекламы'
+
+    const findAndCloseModal = () => {
+        const h1Elements = document.querySelectorAll('h1.blocker__title')
+
+        for (const h1 of h1Elements) {
+            if (h1.textContent.includes(targetText)) {
+                const closeButton = h1.closest('.modal-window')?.querySelector('button.modal-window__close')
+                if (closeButton) {
+                    closeButton.click()
+                    return
+                }
+            }
+        }
+    }
+
+    const observer = new MutationObserver(findAndCloseModal)
+
+    observer.observe(document.body, {
+        childList: true,
+        subtree: true
+    })
+
+    // Проверяем наличие окна сразу при запуске
+    findAndCloseModal()
+})()
